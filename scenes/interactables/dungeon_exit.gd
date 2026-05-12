0eebafb597ef69e47bedc5c298c58e7d0e55c6a7
moran <sharon.ky.moran@gmail.com>
2026-05-11 20:02:38 -0400

    Add dungeon exit UI feedback

--- scenes/interactables/dungeon_exit.gd
+++ scenes/interactables/dungeon_exit.gd
@@ -2,14 +2,20 @@ extends Area3D
 
 @onready var game_ui = get_tree().current_scene.get_node("GameUI")
 
+var level_completed: bool = false
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(body: Node3D) -> void:
+	if level_completed:
+		return
+	
 	if body.name != "Player":
 		return
 		
 	if body.has_collected_all_bones():
+		level_completed = true
 		print("Dungeon complete!")
 		game_ui.update_objective("Dungeon complete!")
 	else: 
